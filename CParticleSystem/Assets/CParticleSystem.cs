f241dc76281442d143b5c90d09b67ce7579765c2
matthew-zhang-306 <matthew.zhang.306@gmail.com>
2021-04-28 21:20:16 -0500

    create some skeleton stuff

---
+++ CParticleSystem/Assets/CParticleSystem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CParticleSystem : MonoBehaviour
+{
+    /*
+    Mesh
+    Num alive particles
+    Free particles (queue)
+    Particle[]
+    Emitter component // 1
+    If particle[] has space, make particle
+    If any in particle[] has time elapsed >= lifetime, empty the spot
+    Do actions based on parameters
+    List of components // 2 through N-1
+    Renderer component // N
+    Creates a mesh out of all particle data
+    Do actions based on parameters
+    Bit ID (100000) - used to detect what components are enabled
+    Map (string -> value) for default values
+    */
+
+    Mesh mesh; // TODO: check if this is actually what you do
+    
+    // particle data
+    HashSet<Particle> aliveParticles;
+    Queue<Particle> deadParticles;
+    int numAliveParticles => aliveParticles.Count;
+
+    // component data
+    [Header("Emission Data")]
+    [SerializeField] private int maxParticles;
+    [SerializeField] private float particlesPerSecond;
+    [SerializeField] private float startLifetime;
+    [SerializeField] private bool isLooping;
+
+    [Header("Renderer Data")]
+    [SerializeField] private Material startMaterial;
+    [SerializeField] private float startSize;
+
+    List<IPSComponent> components;
+    // RendererComponent renderer;
+
+
+    private void Awake() {
+        aliveParticles = new HashSet<Particle>();
+        deadParticles = new Queue<Particle>();
+        components = new List<IPSComponent>();
+    }
+
+    private void Update() {
+        // emit clause
+        
+        // kill clause
+
+        // update all components
+
+        // render
+    }
+}
---
+++ CParticleSystem/Assets/Particle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle
+{
+    private Dictionary<string, object> data;
+
+    public Particle() {
+        data = new Dictionary<string, object>();
+    }
+
+    // returns data[name] cast into type T
+    public T Get<T>(string name) {
+        // TODO
+        return default(T);
+    }
+
+    // stores value inside data under the name
+    public void Set<T>(string name, T value) {
+        // TODO
+    }
+
+    // clears the data so that this particle can be reused
+    public void Reset() {
+        // TODO
+    }
+}
