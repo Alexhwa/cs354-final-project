9276229fb7246ba9b79c5710915ae42148f1dc00
Alexhwa <ah.alswad@gmail.com>
2021-05-02 22:29:46 -0500

    Toggleable orbiting camera added

---
+++ CParticleSystem/Assets/Scripts/CameraPresent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraPresent : MonoBehaviour
+{
+    public KeyCode toggleKey;
+    public KeyCode speedUpKey;
+    public KeyCode speedDownKey;
+
+    private bool toggled;
+    public CinemachineVirtualCamera virtCam;
+    private CinemachineTrackedDolly pathBody;
+
+    public float panSpeed;
+    private float stepSize = .005f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathBody = virtCam.GetCinemachineComponent<CinemachineTrackedDolly>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(toggleKey))
+        {
+            toggled = !toggled;
+        }
+        if (Input.GetKey(speedUpKey))
+        {
+            panSpeed += stepSize;
+        }
+        if (Input.GetKey(speedDownKey))
+        {
+            panSpeed -= stepSize;
+        }
+        if (toggled) {
+            pathBody.m_PathPosition += panSpeed;
+        }
+    }
+}
