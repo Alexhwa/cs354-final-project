322d9bfd6eff759cb1803e0337c6d8f193449821
matthew-zhang-306 <matthew.zhang.306@gmail.com>
2021-05-13 14:58:38 -0500

    made velocity over lifetime relative

--- CParticleSystem/Assets/VelocityOverLifeTimeModule.cs
+++ CParticleSystem/Assets/VelocityOverLifeTimeModule.cs
@@ -4,21 +4,49 @@ using UnityEngine;
 
 public class VelocityOverLifeTimeModule : IModule
 {
-    [SerializeField] private AnimationCurve xVelocity;
-    [SerializeField] private AnimationCurve yVelocity;
-    [SerializeField] private AnimationCurve zVelocity;
+    [SerializeField] private AnimationCurve xVelocity; // corresponds to binormal
+    [SerializeField] private AnimationCurve yVelocity; // corresponds to normal
+    [SerializeField] private AnimationCurve zVelocity; // corresponds to base
 
     public override void InitParticle(Particle particle)
     {
-        particle.Set<Vector3>("Initial Velocity", particle.Get<Vector3>("Velocity"));
+        var baseVelocity = particle.Get<Vector3>("Velocity");
+        particle.Set<Vector3>("Base Velocity", baseVelocity);
+
+        var otherAxis = Vector3.up; 
+        if (baseVelocity.x == 0 && baseVelocity.z == 0) {
+            // edge case
+            otherAxis = -Vector3.forward;
+        }
+
+        var binormalVelocity = Vector3.Cross(otherAxis, baseVelocity);
+        binormalVelocity = binormalVelocity.normalized * baseVelocity.magnitude;
+        particle.Set<Vector3>("Binormal Velocity", binormalVelocity);
+
+        var normalVelocity = Vector3.Cross(baseVelocity, binormalVelocity);
+        normalVelocity = normalVelocity.normalized * baseVelocity.magnitude;
+        particle.Set<Vector3>("Normal Velocity", normalVelocity);
+
+        particle.Set<Vector3>("Velocity",
+            particle.Get<Vector3>("Binormal Velocity") * xVelocity.Evaluate(0) +
+            particle.Get<Vector3>("Normal Velocity") * yVelocity.Evaluate(0) +
+            particle.Get<Vector3>("Base Velocity") * zVelocity.Evaluate(0)
+        );
+        Debug.Log(particle.Get<Vector3>("Binormal Velocity") * xVelocity.Evaluate(0) +
+            particle.Get<Vector3>("Normal Velocity") * yVelocity.Evaluate(0) +
+            particle.Get<Vector3>("Base Velocity") * zVelocity.Evaluate(0));
     }
     public override void UpdateParticles(HashSet<Particle> aliveParticles)
     {
         foreach (Particle p in aliveParticles)
         {
-            var curveX = 1 - p.Get<float>("Lifetime") / p.Get<float>("Total_Lifetime");
-            var originalVel = p.Get<Vector3>("Velocity");
-            p.Set<Vector3>("Velocity", new Vector3(originalVel.x + xVelocity.Evaluate(curveX), originalVel.y + yVelocity.Evaluate(curveX), originalVel.z + zVelocity.Evaluate(curveX)));
+            var curveT = 1 - p.Get<float>("Lifetime") / p.Get<float>("Total_Lifetime");
+
+            p.Set<Vector3>("Velocity",
+                p.Get<Vector3>("Binormal Velocity") * xVelocity.Evaluate(curveT) +
+                p.Get<Vector3>("Normal Velocity") * yVelocity.Evaluate(curveT) +
+                p.Get<Vector3>("Base Velocity") * zVelocity.Evaluate(curveT)
+            );
         }
     }
 }
